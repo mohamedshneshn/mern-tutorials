33c4deee0e8f5a44de6a229befe96184ab193b37
MOHAMED <98075987+mohamedshneshn@users.noreply.github.com>
2024-05-19 13:14:52 -0700

    ff

---
+++ frontend/folder-structure.js
@@ -0,0 +1,31 @@
+/*
+React-folder-structure
+----------------------
+client
+    node_modules                       //contains all the dependencies
+    public                            //contains the static files
+        index.html
+    src                              //contains the source code
+        assets
+            images
+        components
+            Header.js
+            Footer.js
+            Home.js
+            About.js
+            Contact.js
+        App.js
+        index.js
+    package.json
+    package-lock.json
+    README.md
+    .gitignore
+    .env
+    .env.example
+
+
+
+
+
+
+*/
